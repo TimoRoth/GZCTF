9450a6eab644178ed7f4c0dd0789fc2bc86048f3
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2025-04-16 23:46:55 +0200

    Initial simple implementation of docker compose based manager

--- src/GZCTF/Services/Container/ExtraManager/DockerComposeManager.cs
+++ src/GZCTF/Services/Container/ExtraManager/DockerComposeManager.cs
@@ -9,6 +9,7 @@ namespace GZCTF.Services.Container.Manager;
 
 public class DockerComposeManager : IContainerManager
 {
+    readonly DockerClient _client;
     readonly ILogger<DockerComposeManager> _logger;
     readonly DockerMetadata _meta;
     readonly DockerManager _fallbackManager;
@@ -17,6 +18,7 @@ public class DockerComposeManager : IContainerManager
     {
         _logger = logger;
         _meta = provider.GetMetadata();
+        _client = provider.GetProvider();
         _fallbackManager = new DockerManager(provider, fallbackLogger);
 
         logger.SystemLog(StaticLocalizer[nameof(Resources.Program.ContainerManager_DockerComposeMode)], TaskStatus.Success, LogLevel.Debug);
@@ -31,6 +33,16 @@ public class DockerComposeManager : IContainerManager
             return;
         }
 
+        //TODO: Exception handling
+        await LaunchHelper("docker",
+            ["compose", "--file", "-", "--project-name", container.ContainerId, "--progress", "plain", "down", "--remove-orphans", "--volumes"],
+            new Dictionary<string, string>
+            {
+                { "DOCKER_HOST", _client.Configuration.EndpointBaseUri.ToString() },
+                //TODO: Pass resource limit, the token/flag variables and maybe the desired port to be exposed
+            },
+            container.Image);
+
         container.Status = ContainerStatus.Destroyed;
     }
 
@@ -40,8 +52,135 @@ public class DockerComposeManager : IContainerManager
         if (!config.Image.Trim('\n', '\r').Contains('\n'))
             return await _fallbackManager.CreateContainerAsync(config, token);
 
+        string name = $"{config.TeamId}_{config.ChallengeId}";
 
-        Models.Data.Container res = new Models.Data.Container();
-        return res;
+        //TODO: sign into registries
+
+        //TODO: Exception handling
+        //TODO: Move this to a safe workdir
+        await LaunchHelper("docker",
+            ["compose", "--file", "-", "--project-name", name, "--progress", "plain", "up", "-d", "--wait", "--pull", "missing"],
+            new Dictionary<string, string>
+            {
+                { "DOCKER_HOST", _client.Configuration.EndpointBaseUri.ToString() },
+            },
+            config.Image);
+
+        //TODO: test if service with name "main" exists, since further info will be drawn from it. Or try to parse first via "docker compose config"
+
+        Models.Data.Container container = new Models.Data.Container
+        {
+            ContainerId = name,
+            Image = config.Image,
+            StartedAt = DateTimeOffset.UtcNow, // Has to be UTC for Postgres
+            IP = "127.0.0.42", //TODO
+            Port = config.ExposedPort,
+            IsProxy = !_meta.ExposePort,
+            Status = ContainerStatus.Running,
+        };
+
+        container.ExpectStopAt = container.StartedAt + TimeSpan.FromHours(2);
+
+        if (!_meta.ExposePort)
+            return container;
+
+        container.PublicPort = 1234; // TODO
+
+        if (!string.IsNullOrEmpty(_meta.PublicEntry))
+            container.PublicIP = _meta.PublicEntry;
+
+        return container;
+    }
+
+    private async Task<string> LaunchHelper(string command, string[] arguments, Dictionary<string, string> env, string? input = null)
+    {
+        //TODO: maybe somehow cache this
+        if (!Path.IsPathRooted(command) && !command.Contains(Path.DirectorySeparatorChar) && !command.Contains(Path.AltDirectorySeparatorChar) && !File.Exists(command))
+        {
+            string[] path = Environment.GetEnvironmentVariable("PATH")!.Split(Path.PathSeparator);
+            string[] exts = Environment.GetEnvironmentVariable("PATHEXT")?.Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+
+            foreach (string dir in path)
+            {
+                string? fullCommand = Path.Combine(dir, command);
+                if (File.Exists(fullCommand))
+                {
+                    command = fullCommand;
+                    break;
+                }
+
+                fullCommand = null;
+                foreach (string ext in exts)
+                {
+                    string fullCommandExt = Path.Combine(dir, command + ext);
+                    if (File.Exists(fullCommandExt))
+                    {
+                        command = fullCommand = fullCommandExt;
+                        break;
+                    }
+                }
+
+                if (fullCommand != null)
+                    break;
+            }
+        }
+
+        using var proc = new Process();
+        proc.StartInfo = new ProcessStartInfo
+        {
+            FileName = command,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            RedirectStandardInput = true,
+            CreateNoWindow = true,
+        };
+
+        foreach (string arg in arguments)
+            proc.StartInfo.ArgumentList.Add(arg);
+
+        foreach (var pair in env)
+            proc.StartInfo.Environment.Add(pair.Key, pair.Value);
+
+        proc.EnableRaisingEvents = true;
+
+        StringBuilder resBuilder = new StringBuilder();
+        DataReceivedEventHandler handler = (sender, data) => { resBuilder.AppendLine(data.Data); };
+        proc.OutputDataReceived += handler;
+        proc.ErrorDataReceived += handler;
+
+        try
+        {
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
+
+            using (var writer = proc.StandardInput)
+                if (input != null)
+                    await writer.WriteAsync(input);
+
+            await proc.WaitForExitAsync();
+
+            if (proc.ExitCode != 0)
+                throw new LaunchException(proc.ExitCode, resBuilder.ToString());
+
+            return resBuilder.ToString();
+        }
+        finally
+        {
+            proc.OutputDataReceived -= handler;
+            proc.ErrorDataReceived -= handler;
+        }
+    }
+
+    private class LaunchException : Exception
+    {
+        public int ExitCode { get; private set; }
+
+        public LaunchException(int code, string message)
+            : base(message)
+        {
+            ExitCode = code;
+        }
     }
 }
